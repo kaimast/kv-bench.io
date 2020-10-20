6c070d117869360039ec433dbf7a2660d26964bb
Kai Mast <kaimast@cs.wisc.edu>
2020-10-20 14:21:35 -0500

    Checkpoint

--- Makefile
+++ Makefile
@@ -1,8 +1,17 @@
-.PHONY: prepare
+.PHONY: prepare builddir benchdir run
 
 BUILD_DIR=build
+BENCH_DIR=bench
 
-prepare:
+builddir:
 	mkdir -p ${BUILD_DIR}
+
+prepare: builddir
 	cd ${BUILD_DIR} && git clone git@github.com:kaimast/kv-bench.git
 	cd ${BUILD_DIR}/kv-bench && cargo build --release --features=all
+
+benchdir:
+	mkdir -p ${BENCH_DIR}
+
+run: benchdir
+	cd ${BENCH_DIR} && ../write_throughput.py --run --plot
--- write_throughput.py
+++ write_throughput.py
@@ -1,6 +1,26 @@
 #! /usr/bin/python3
 
-batch_sizes = [1, 100, 1000, 10000]
+import argparse
 
-for batch_size in batch_sizes:
-    
+from subprocess import call
+
+BIN_PATH="../build/kv_bench/target/release/kv_bench"
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--run", action="store_true", default=False)
+    parser.add_argument("--plot", action="store_true", default=False)
+
+    args = parser.parse_args()
+
+    if args.run:
+        run()
+
+def run():
+    batch_sizes = [1, 100, 1000, 10000]
+
+    for batch_size in batch_sizes:
+        call([BIN_PATH])
+
+if __name__ == "__main__":
+    main()
