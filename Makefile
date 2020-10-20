a4b65886fc9368ce73daa683f692a5aa1a371334
Kai Mast <kaimast@cs.cornell.edu>
2020-10-20 19:06:28 -0400

    Plotting script

--- Makefile
+++ Makefile
@@ -1,17 +1,22 @@
-.PHONY: prepare builddir benchdir run
+.PHONY: prepare builddir benchdir run plot
 
 BUILD_DIR=build
 BENCH_DIR=bench
+
+REPO=git@github.com:kaimast/kv-bench.git
 
 builddir:
 	mkdir -p ${BUILD_DIR}
 
 prepare: builddir
-	cd ${BUILD_DIR} && git clone git@github.com:kaimast/kv-bench.git
-	cd ${BUILD_DIR}/kv-bench && cargo build --release --features=all
+	if [ ! -d ${BUILD_DIR}/kv-bench ]; then cd ${BUILD_DIR} && git clone ${REPO}; fi
+	cd ${BUILD_DIR}/kv-bench && git pull && cargo install --features=all --path=.
 
 benchdir:
 	mkdir -p ${BENCH_DIR}
 
 run: benchdir
-	cd ${BENCH_DIR} && ../write_throughput.py --run --plot
+	cd ${BENCH_DIR} && ../write_throughput.py --run
+
+plot: benchdir
+	cd ${BENCH_DIR} && ../write_throughput.py --plot
--- write_throughput.py
+++ write_throughput.py
@@ -1,10 +1,18 @@
 #! /usr/bin/python3
 
 import argparse
+import json
+import matplotlib.pyplot as plt
+import numpy as np
 
+from pandas import DataFrame, read_csv
 from subprocess import call
 
-BIN_PATH="../build/kv_bench/target/release/kv_bench"
+BIN_PATH = "kv-bench"
+OUT_FILE = "data.json"
+RESULTS = "results.csv"
+
+BACKENDS = ["leveldb", "rocksdb"]
 
 def main():
     parser = argparse.ArgumentParser()
@@ -16,11 +24,69 @@ def main():
     if args.run:
         run()
 
-def run():
-    batch_sizes = [1, 100, 1000, 10000]
+    if args.plot:
+        plot()
 
-    for batch_size in batch_sizes:
-        call([BIN_PATH])
+def run():
+    workload = "write_only"
+    batch_sizes = [1, 100, 1000, 10000]
+    total_ops = 50000
+
+    columns = ["backend", "batch_size", "throughput"]
+    results = DataFrame([], columns=columns)
+
+    num_iterations = 5
+    num_runs = len(backends) * len(batch_sizes) * num_iterations
+    count = 0
+
+    for backend in BACKENDS:
+        for batch_size in batch_sizes:
+            for _ in range(num_iterations):
+                num_ops = int(total_ops / batch_size)
+
+                call([BIN_PATH, "--backend="+backend, "--workload="+workload, "--batch_size=%i"%batch_size,
+                    "--sync=true", "--outfile="+OUT_FILE, "--num_ops=%i"%num_ops])
+
+                with open(OUT_FILE) as jfile:
+                    data = json.load(jfile)
+
+                print(str(data))
+
+                row = DataFrame([[backend, batch_size, data["throughput"]]], columns=columns)
+                results = results.append(row)
+
+                results.to_csv(RESULTS, index=False)
+
+                count += 1
+                print("Progress: %i out of %i" % (count, num_runs))
+
+    print("Done")
+
+def plot():
+    df = read_csv(RESULTS)
+
+    fig = plt.figure()
+    ax = fig.add_subplot(1, 1, 1)
+
+    for backend in BACKENDS:
+        data = df[df["backend"] == backend]
+        keys = data.batch_size.unique()
+        means = []
+        sdevs = []
+
+        for batch_size in keys:
+            vals = data[data["batch_size"] == batch_size]
+            means.append(np.mean(vals.throughput))
+            sdevs.append(np.std(vals.throughput))
+
+        ax.errorbar(keys, means, yerr=sdevs, label=backend)
+
+    ax.set_xlabel("batch size")
+    ax.set_ylabel("throughput")
+
+    plt.tight_layout()
+    fig.legend()
+    fig.savefig("results.pdf")
 
 if __name__ == "__main__":
     main()
